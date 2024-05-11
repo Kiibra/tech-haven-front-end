f8ba20c4279b869fdb1026b36ad97af392d0fe71
kiibra <kibra99@gmail.com>
2024-05-11 17:26:42 -0400

    complete deviceList functionality

--- src/pages/DeviceList/DeviceList.jsx
+++ src/pages/DeviceList/DeviceList.jsx
@@ -1,11 +1,15 @@
-
+//components 
+import DeviceCard from '../../components/DeviceCard/DeviceCard' 
 //CSS
 import styles from './DeviceList.module.css'
 
-const DeviceList = () => {
+const DeviceList = (props) => {
   return (  
     <main className={styles.container}>
-      <h1>Device List</h1>
+      <p>Device List</p>
+      {props.devices.map(device => (
+        <DeviceCard key={device._id} device={device}/>
+      ))}
     </main>
   )
 }
