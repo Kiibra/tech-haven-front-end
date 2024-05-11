bf7febb7d7fe1d33d61dd91a7b87a19f81289e0d
Seun Dada <seun.dada777@gmail.com>
2024-05-11 17:04:22 -0400

    add components for the app

---
+++ src/components/DeviceCard/DeviceCard.jsx
@@ -0,0 +1,12 @@
+//CSS
+import styles from './DeviceCard.module.css'
+
+const DeviceCard = () => {
+  return (  
+    <main className={styles.container}>
+      <h1>Device Card</h1>
+    </main>
+  )
+}
+
+export default DeviceCard
