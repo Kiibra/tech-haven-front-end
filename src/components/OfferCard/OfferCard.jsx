8f291ae37a84a105ab0909b5edc4c15643d53294
Seun Dada <seun.dada777@gmail.com>
2024-05-13 11:53:37 -0400

    setup offerCard and offers component

--- src/components/OfferCard/OfferCard.jsx
+++ src/components/OfferCard/OfferCard.jsx
@@ -1,10 +1,35 @@
+// npm modules
+import { NavLink } from "react-router-dom"
 
-const DeviceCard = () => {
+// components
+import AuthorInfo from "../AuthorInfo/AuthorInfo"
+import Icon from "../Icon/Icon"
+
+const OfferCard = ({ offer, user, deviceId }) => {
   return (  
     <main>
-      <h1>Device Card</h1>
+      <header>
+        <AuthorInfo content={offer} />
+        <p>{offer.value}</p>
+        {offer.author._id === user.profile &&
+          <>
+            <NavLink 
+              to={`/devices/${deviceId}/offers/edit`}
+              state={offer}
+            >
+              <Icon category="Edit" />
+            </NavLink>
+            <button>
+              <Icon category="Trash" />
+            </button>
+          </>
+        }
+      </header>
+      <span>
+        <p>{offer.comment}</p>
+      </span>
     </main>
   )
 }
 
-export default DeviceCard
+export default OfferCard
--- src/components/Offers/Offers.jsx
+++ src/components/Offers/Offers.jsx
@@ -1,12 +1,20 @@
+import OfferCard from "../OfferCard/OfferCard"
 
 const Offers = (props) => {
   
   if (!props.offers.length) return <h4>No Offers</h4>
   
-  return (  
-    <main>
-      <h1>Device Offers</h1>
-    </main>
+  return (
+    <>
+      {props.offers.map(offer =>
+        <OfferCard
+          key={offer._id}
+          offer={offer}
+          user={props.user}
+          deviceId={props.deviceId}
+        />
+      )}
+    </>
   )
 }
 
